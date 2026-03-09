997f19fded48620977a764e03200f7afa6e0ac0f
Anurag Kumar <anuragkumar@PSG-MAC-00498.local>
2026-03-09 11:01:06 +0530

    Feedback Implementation

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,57 @@
+using System;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        private int _score = 0;
+        private int _highScore = 0;
+
+        [SerializeField] private float scoreUpdateRate = 0.5f;
+        [SerializeField] private Text scoreText;
+        [SerializeField] private Text highScoreText;
+
+        private void OnEnable()
+        {
+            StartMenuManager.OnGameStart += OnGameStart;
+            GameplayManager.OnGameOver += OnGameOver;
+        }
+
+        private void OnDisable()
+        {
+            StartMenuManager.OnGameStart -= OnGameStart;
+            GameplayManager.OnGameOver -= OnGameOver;
+        }
+
+        private void Awake()
+        {
+            _highScore = PlayerPrefs.GetInt("HighScore");
+            highScoreText.text = "High Score: " + _highScore;
+        }
+
+        private void OnGameStart()
+        {
+            _score = 0;
+            InvokeRepeating(nameof(UpdateScore), 0f, scoreUpdateRate);
+        }
+
+        private void OnGameOver()
+        {
+            CancelInvoke(nameof(UpdateScore));
+        }
+
+        private void UpdateScore()
+        {
+            _score++;
+            scoreText.text = "Score: " + _score;
+            _highScore = Math.Max(_score, _highScore);
+            highScoreText.text = "High Score: " + _highScore;
+        
+            PlayerPrefs.SetInt("HighScore", _highScore);
+        }
+    
+    }
+}
---
+++ Assets/Scripts/Obstacles/ObstacleSpawner.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UI;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Obstacles
+{
+    public class ObstacleSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject obstaclePrefab;
+        [SerializeField] private float baseSpawnDuration;
+        [SerializeField] private float destroyDelay;
+    
+        private Coroutine _spawnObstacleCoroutine;
+
+        private void OnGameStart()
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+            _spawnObstacleCoroutine =  StartCoroutine(SpawnObstacleCoroutine());
+        }
+
+        private void OnGameOver()
+        {
+            StopCoroutine(_spawnObstacleCoroutine);
+        
+        }
+
+        private void SubscribeEvents()
+        {
+            StartMenuManager.OnGameStart += OnGameStart;
+            GameplayManager.OnGameOver += OnGameOver;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            StartMenuManager.OnGameStart -= OnGameStart;
+            GameplayManager.OnGameOver -= OnGameOver;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+    
+
+        private IEnumerator SpawnObstacleCoroutine()
+        {
+            while (true)
+            {
+                var someRandomOffset = Random.Range(-baseSpawnDuration / 2, baseSpawnDuration / 2);
+                yield return new WaitForSeconds(baseSpawnDuration + someRandomOffset);
+                SpawnObstacle();
+            }
+        }
+    
+
+        private void SpawnObstacle()
+        {
+            var obstacle = Instantiate(obstaclePrefab, transform);
+            Destroy(obstacle, destroyDelay);
+        }
+    }
+}
